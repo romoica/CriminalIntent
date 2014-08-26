3547d5d9f36fbbfaa56e73ec885fe98e76036805
romoica <romoica@gmail.com>
2014-08-26 10:33:27 +0200

    test

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -2,6 +2,10 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
@@ -10,6 +14,7 @@ import java.util.List;
  */
 public class CrimeListFragment extends ListFragment {
 
+    private static final String TAG = "CrimeListFragment";
     private List<Crime> mCrimes;
 
     @Override
@@ -17,5 +22,17 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.getInstance(getActivity()).getCrimes();
+        ArrayAdapter<Crime> adapter =
+                new ArrayAdapter<Crime>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        mCrimes);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.i(TAG, c.getTitle() + " was clicked");
     }
 }
